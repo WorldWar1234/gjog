94ea58c80cd7d99b40a89111babc622f8158b642
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-07-26 15:56:30 +0530

    Update proxy.js

--- src/proxy.js
+++ src/proxy.js
@@ -15,7 +15,11 @@ async function proxy(req, res) {
   /*
    * Avoid loopback that could causing server hang.
    */
-  
+    if (
+    req.headers["via"] == "1.1 bandwidth-hero" &&
+    ["127.0.0.1", "::1"].includes(req.headers["x-forwarded-for"] || req.ip)
+  )
+    return redirect(req, res);
   try {
     let origin = await undici.request(req.params.url, {
       headers: {
