12295513a598088800aa8f95ef23580a358b1f95
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-07-30 12:18:16 +0530

    Update shouldCompress.js

--- src/shouldCompress.js
+++ src/shouldCompress.js
@@ -4,19 +4,16 @@ const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 100;
 function shouldCompress(req) {
   const { originType, originSize, webp } = req.params;
 
-  if (typeof originType !== 'string' || !originType.startsWith('image')) {
+  if (!originType.startsWith('image')) {
     return false;
   }
-
-  if (isNaN(originSize) || originSize === 0) {
+  if (originSize === 0) {
     return false;
   }
-
-  if (webp && (isNaN(originSize) || originSize < MIN_COMPRESS_LENGTH)) {
+  if (webp && originSize < MIN_COMPRESS_LENGTH) {
     return false;
   }
-
-  if (!webp && (originType.endsWith('png') || originType.endsWith('gif')) && (isNaN(originSize) || originSize < MIN_TRANSPARENT_COMPRESS_LENGTH)) {
+  if (!webp && (originType.endsWith('png') || originType.endsWith('gif')) && originSize < MIN_TRANSPARENT_COMPRESS_LENGTH) {
     return false;
   }
 
