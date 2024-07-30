15dd130fdf9e842fd6c196bbea1a44161ad3ea14
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-07-30 13:30:45 +0530

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -1,37 +1,41 @@
 const sharp = require('sharp');
 const shouldCompress = require('./shouldCompress');
-const setOriginHeaders = require('./setOriginHeaders');
+const copyHeaders = require('./copyHeaders');
 
 function compress(req, res, imageUrl) {
-  setOriginHeaders(req, res, () => {
-    if (shouldCompress(req)) {
-      sharp(imageUrl)
-        .grayscale(req.params.grayscale)
-        .toFormat(req.params.webp ? 'webp' : 'jpeg', {
-          quality: req.params.quality,
-          progressive: true,
-          optimizeScans: true
-        })
-        .toBuffer((err, output, info) => {
-          if (err) {
-            console.error(err);
-            return res.status(500).end();
+  if (shouldCompress(req)) {
+    sharp(imageUrl)
+      .grayscale(req.params.grayscale)
+      .toFormat(req.params.webp ? 'webp' : 'jpeg', {
+        quality: req.params.quality,
+        progressive: true,
+        optimizeScans: true
+      })
+      .toBuffer((err, output, info) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).end();
+        }
+        console.log('Output buffer:', output.length);
+        const format = req.params.webp ? 'webp' : 'jpeg';
+        const manipulator = (key, value) => {
+          if (key === 'content-type') {
+            return `image/${format}`;
           }
-          console.log('Output buffer:', output.length);
-          const format = req.params.webp ? 'webp' : 'jpeg';
-          res.setHeader('content-type', `image/${format}`);
-          res.setHeader('content-length', info.size);
-          res.setHeader('content-encoding', 'identity');
-          res.setHeader('x-original-size', req.params.originSize);
-          res.setHeader('x-bytes-saved', req.params.originSize - info.size);
-          console.log('Headers:', res.getHeaders());
-          console.log('Sending response...');
-          res.status(200).send(output);
-        });
-    } else {
-      return res.redirect(imageUrl);
-    }
-  });
+          return value;
+        };
+        copyHeaders(req, res, manipulator);
+        res.setHeader('content-length', info.size);
+        res.setHeader('content-encoding', 'identity');
+        res.setHeader('x-original-size', req.params.originSize);
+        res.setHeader('x-bytes-saved', req.params.originSize - info.size);
+        console.log('Headers:', res.getHeaders());
+        console.log('Sending response...');
+        res.status(200).send(output);
+      });
+  } else {
+    return res.redirect(imageUrl);
+  }
 }
 
 module.exports = compress;
