8636aa53b7306c72d1d99df7b007530d7c7fe477
WorldWar1234 <dsfsdfsgvd@proton.me>
2024-07-30 12:11:41 +0530

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -2,27 +2,26 @@ const sharp = require('sharp');
 const shouldCompress = require('./shouldCompress');
 
 function compress(req, res, imageUrl) {
-  if (!shouldCompress(req)) {
-    // Redirect to the original image URL without compression
+  if (shouldCompress(req)) {
+    sharp(imageUrl)
+      .grayscale(req.params.grayscale)
+      .toFormat(req.params.webp ? 'webp' : 'jpeg', {
+        quality: req.params.quality,
+        progressive: true,
+        optimizeScans: true
+      })
+      .toBuffer((err, output, info) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).end();
+        }
+        res.setHeader('content-type', `image/${req.params.webp ? 'webp' : 'jpeg'}`);
+        res.setHeader('content-length', info.size);
+        res.status(200).send(output);
+      });
+  } else {
     return res.redirect(imageUrl);
   }
-
-  sharp(imageUrl)
-    .grayscale(req.params.grayscale)
-    .toFormat(req.params.webp ? 'webp' : 'jpeg', {
-      quality: req.params.quality,
-      progressive: true,
-      optimizeScans: true
-    })
-    .toBuffer((err, output, info) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).end();
-      }
-      res.setHeader('content-type', `image/${req.params.webp ? 'webp' : 'jpeg'}`);
-      res.setHeader('content-length', info.size);
-      res.status(200).send(output);
-    });
 }
 
 module.exports = compress;
